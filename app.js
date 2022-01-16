b098b715b906cfbd3e3c88a62aac972bedb07da5
tmgAzit <azit.ghising@gmail.com>
2022-01-16 22:55:07 +1100

    Test mongoose to create DB and collections.

---
+++ app.js
@@ -0,0 +1,49 @@
+const Mongoose = require('mongoose');
+Mongoose.connect('mongodb://localhost:27017/fruitsDB');
+console.log('it is running.');
+
+const peopleSchema = new Mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const People = Mongoose.model('People', peopleSchema);
+
+const people = new People({
+  name: 'Ajit',
+  age: 24,
+});
+
+// people.save();
+
+const fruitSchema = new Mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = Mongoose.model('Fruit', fruitSchema);
+
+const apple = new Fruit({
+  name: 'Apple',
+  rating: 6,
+  review: 'Yummy',
+});
+const orange = new Fruit({
+  name: 'Orange',
+  rating: 5,
+  review: 'Yummy',
+});
+const mango = new Fruit({
+  name: 'Mango',
+  rating: 8,
+  review: 'Yummy',
+});
+
+Fruit.insertMany([apple, orange, mango], function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('All the fruits are successfully saved.');
+  }
+});
